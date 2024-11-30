9258673781396420b2a79bc3e04f9ce2deb49cd6
Macktireh <abdimack97@gmail.com>
2024-11-30 23:53:11 +0100

    first commit

---
+++ src/main/java/dev/macktireh/liquibase/entities/Post.java
@@ -0,0 +1,36 @@
+package dev.macktireh.liquibase.entities;
+
+import java.sql.Date;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @OneToMany(mappedBy = "post")
+    private Set<Comment> comments;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+}
